400b79eaa870acdc5c2dbe21433a7de7b7c51183
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-18 14:56:12 +0200

    commit 3 - stamp FizzBuzz

--- src/org/lessons/java/fizzbuzz/fizzbuzz.java
+++ src/org/lessons/java/fizzbuzz/fizzbuzz.java
@@ -5,11 +5,13 @@ public class fizzbuzz {
         int number = 100;
         System.out.print("Number from 1 - " + number + " are: ");
         for (int i = 0; i <= number; i++) {
-            if (i % 3 == 0){
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }else if (i % 3 == 0) {
                 System.out.println("Fizz");
             }else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            }else{
+            }else {
                 System.out.println(i + " ");
             }
         }
