5e018273f3724b555bd689ec30b8e4777e457d0b
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-18 17:14:09 +0200

    commit 5 - exercise Calcolo biglietto del treno

--- src/org/lessons/java/fizzbuzz/fizzbuzz.java
+++ src/org/lessons/java/fizzbuzz/fizzbuzz.java
@@ -1,5 +1,11 @@
 package org.lessons.java.fizzbuzz;
 
+/*
+Esercizio 1: FizzBuzz
+Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
+
 public class fizzbuzz {
     public static void main(String[] args) {
         int number = 100;
---
+++ src/org/lessons/java/train/ticketrain.java
@@ -0,0 +1,43 @@
+package org.lessons.java.train;
+
+import java.util.Scanner;
+
+public class ticketrain {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        //prezzo per km
+        double priceForKm = 0.21;
+        // sconto per gli under 18
+        double discountunder18 = 0.20;
+        // sconto per gli over 65
+        double discountover65 = 0.40;
+
+        //Richiesta - Quanti km devi percorrere
+        System.out.print("How many km do you have to travel(Km)?: ");
+        int travelKm = scan.nextInt();
+
+        //Richiesta - Qual'è la tua età
+        System.out.print("What's your age?: ");
+        int agePerson = scan.nextInt();
+
+        //Condizioni  - Età
+        if (agePerson >= 18 && agePerson <= 65){
+            //Se l'età e tra 18 e 65 anni, compresi, allora:
+            double resultPrice = travelKm * priceForKm;
+            System.out.print("Il prezzo del tuo biglietto: " + resultPrice + " €");
+        }
+        if (agePerson < 18){
+            //Se l'età è minore di 18 anni allora:
+            double finalResult =  (travelKm * priceForKm) * discountunder18;
+            System.out.print("Il prezzo del tuo biglietto: " + finalResult + " €");
+        } else if(agePerson > 65) {
+            //Se l'età è maggiore di 65 anni allora:
+            double finalResult =  (travelKm * priceForKm) * discountover65;
+            System.out.print("Il prezzo del tuo biglietto: " + finalResult + " €");
+        }
+
+        scan.close();
+    }
+}
