a4901f6175b92245443469bdcc8a4987073ff655
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-18 14:52:31 +0200

    commit 2 - stamp Fizz and stamp Buzz

---
+++ src/org/lessons/java/fizzbuzz/fizzbuzz.java
@@ -0,0 +1,17 @@
+package org.lessons.java.fizzbuzz;
+
+public class fizzbuzz {
+    public static void main(String[] args) {
+        int number = 100;
+        System.out.print("Number from 1 - " + number + " are: ");
+        for (int i = 0; i <= number; i++) {
+            if (i % 3 == 0){
+                System.out.println("Fizz");
+            }else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
